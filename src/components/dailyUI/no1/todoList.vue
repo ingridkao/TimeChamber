f2906b3a74ee9519d24836383eb246ba11a0ffc1
ingridkao <ingrid7613@gmail.com>
2018-06-07 18:28:56 +0800

    Add lodash & fonticon

--- src/components/dailyUI/no1/todoList.vue
+++ src/components/dailyUI/no1/todoList.vue
@@ -23,31 +23,81 @@
                         @keyup.enter="actionAddTodo"/>
                     <button class="createTodo" @click="actionAddTodo" :disabled="todoTitle == ''">Add Task</button>
                 </div>
-                <div class="chartBox"></div>
+
+                <div class="chartBox">
+                    <div class="chartGauge">
+                        <svg viewBox="0 0 173 173" preserveascpectratio="xMinYMin Meet">
+                            <circle class="rail" cx="86.5" cy="86.5" r="73.5"></circle>
+                            <path class="progress" d="M 86 13 a 73.5 73.5 0 1 0.1 0 Z" :style="{ strokeDashoffset: completeOffset(completeRatio) + 'px' }"></path>
+                            <linearGradient id="linearGradient" x1="0" y1="0" x2="1" y2="1">
+                                <stop offset="0" stop-color="#fee4dd"></stop>
+                                <stop offset="1" stop-color="#5761bb"></stop>
+                            </linearGradient>
+                        </svg>
+                    </div>
+                    <div class="circleScreen" @click="toggleChart.toggle = !toggleChart.toggle">
+                        <div class="complete" :class="{ active: toggleChart.toggle }">
+                            <h4>{{completeRatio}}%</h4>
+                            <p>Completed</p>
+                        </div>
+                        <div class="uncompleted" :class="{ active: !toggleChart.toggle }">
+                            <h4>{{uncompleteRatio}}%</h4>
+                            <p>Uncompleted</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="rightSlide">
                 <header class="actionGroup">
-                    <button>All</button>
+                    <button @click="filterTodos('all')">All</button>
                     <div class="rightGroup">
-                        <button>
-                            Incomplete
+                        <button @click="filterTodos('uncomplete')">
+                            Uncompleted
                         </button>
-                        <button>
-                            Complete
+                        <button @click="filterTodos('complete')">
+                            Completed
                         </button>
                     </div>
                 </header>
-                <div class="todoGroup">
+                <div class="todoGroup scroll scrollY">
                     <ul>
                         <li v-if="todoTask == ''" class="nodata">
-                            <button>●</button>
                             <h6>Body building</h6>
                             <p>Go Work Out</p>
                         </li>
-                        <li v-else v-for="(todo, index) in todoTask">
-                            <button>●</button>
-                            <h6>{{ todo.title }}</h6>
-                            <p>{{ todo.content }}</p>
+                        <li v-else v-for="(todo, index) in todoList">
+                            <div>
+                                <button class="checkbox" :class="{ check: todo.complete }" @click="changeComplete(index)">
+                                    <font-awesome-icon icon="check-circle" />
+                                </button>
+                                <div>
+                                    <h6>{{ todo.title }}</h6>
+                                    <p>{{ todo.text }}</p>
+                                </div>
+
+                                <button class="priority" @click="priorityTodo(index)" :style="{ color: priorityColor(index, todo.priority)}">
+                                    <font-awesome-icon icon="star"/>
+                                </button>
+                                <button class="delete" @click="deleteTodo(index)">
+                                    <font-awesome-icon icon="trash"/>
+                                </button>
+                                <button class="edit" @click="editToggle(index)">
+                                    <font-awesome-icon icon="edit"/>
+                                </button>
+                            </div>
+                            <div class="collapse" :class="{open}">
+                                <input
+                                    type="text"
+                                    placeholder="Title"
+                                    v-model.trim="todoList[index]['title']"/>
+                                <input
+                                    type="text"
+                                    placeholder="Somethings..."
+                                    v-model.trim="todoList[index]['text']"/>
+                                <!-- <button class="alarm">
+                                    <font-awesome-icon icon="bell"/>
+                                </button> -->
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -67,15 +117,23 @@ const todoStorage = {
         sessionStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
     }
 }
+
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 export default {
-    name: 'todoList',
+    name: 'ToDoList',
     data() {
         return {
             openAction: false,
+            toggleChart: {
+                toggle: true,
+                completeRatio: 0,
+                uncompleteRatio: 0
+            },
             todoTitle:'',
             todoContent: '',
             todoTask: todoStorage.fetch(),
-            visibility: 'all'
+            todoList: [],
+            visibility: 'all',
         }
     },
     watch: {
@@ -85,12 +143,32 @@ export default {
             }
         }
     },
-    // computed: {
-    //     filteredTodos: function () {
-    //         return filters[this.visibility](this.todos);
-    //     }
-    // },
+    computed: {
+        completeRatio() {
+            if(this.todoTask.length == 0) return 0;
+            return Math.round((_.filter(this.todoTask, 'complete').length) / this.todoTask.length * 1000) / 10;
+        },
+        uncompleteRatio() {
+            if(this.todoTask.length == 0) return 0;
+            return Math.round((_.filter(this.todoTask, function(o) { return !o.complete; }).length) / this.todoTask.length * 1000) / 10;
+        }
+    },
+    components: {
+        FontAwesomeIcon
+    },
     methods: {
+        completeOffset(ratio) {
+            return (Math.round(ratio)/100)*462 - 462;
+        },
+        priorityColor(index, lv) {
+            if(lv == 0){
+                return 'rgba(255, 255, 255, 0.25)';
+            }else if(lv > 5){
+                this.todoTask[index].priority = 0;
+            }else{
+                return 'rgba(254, 228, 221, ' + (lv * 0.2) + ')';
+            }
+        },
         actionAddTodo() {
             if(this.todoTitle === ""){
                 return false;
@@ -99,8 +177,10 @@ export default {
                 this.todoTask.push({
                     title: this.todoTitle,
                     text: this.todoContent,
-                    complete: false
+                    complete: false,
+                    priority: 0
                 });
+                this.todoList = _.cloneDeep(this.todoTask);
             }
             this.todoTitle = "";
             this.todoContent = "";
@@ -109,22 +189,27 @@ export default {
             this.visibility = filter;
             switch(filter) {
                 case 'all':
-                    return todoTask;
+                    this.todoList = this.todoTask;
                     break;
                 case 'complete':
-                    return todoTask.filter(function(todo) {
-                        return todo.complete;
-                    });
+                    this.todoList = _.filter(this.todoTask, 'complete');
                 break;
-                case 'incomplete':
-                    return todoTask.filter(function(todo) {
-                        return !todo.complete;
-                    });
+                case 'uncomplete':
+                    this.todoList = _.filter(this.todoTask, function(o) { return !o.complete; });
                 break;                    
             }
         },
+        changeComplete: function(index) {
+            this.todoTask[index].complete = !this.todoTask[index].complete;
+            this.todoList = _.cloneDeep(this.todoTask);
+        },
+        priorityTodo: function(index) {
+            this.todoTask[index].priority = this.todoTask[index].priority + 1;
+            this.todoList = _.orderBy(this.todoTask, 'priority', 'asc');
+        },
         deleteTodo: function(index) {
-            this.todos.splice(index, 1);
+            this.todoTask.splice(index, 1);
+            this.todoList = _.cloneDeep(this.todoTask);
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
+import fontawesome from '@fortawesome/fontawesome'
+import faBell from '@fortawesome/fontawesome-free-solid/faBell'
+import faCircle from '@fortawesome/fontawesome-free-solid/faCircle'
+import faCheckCircle from '@fortawesome/fontawesome-free-solid/faCheckCircle'
+import faStar from '@fortawesome/fontawesome-free-solid/faStar'
+import faTrash from '@fortawesome/fontawesome-free-solid/faTrash'
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit'
+
+fontawesome.library.add(faBell, faCircle, faCheckCircle, faStar, faTrash, faEdit)
+
+window._ = require('lodash')
+
 new Vue({
   el: '#app',
   router,
